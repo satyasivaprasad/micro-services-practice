843f504cbe98784e085576acd3ca4c535e93a356
satyasivaprasad <satyasivaprasad.j@gmail.com>
2023-04-07 14:10:52 +0530

    Circute breaker integration

--- order-service/src/main/java/ms/learnings/orderservice/controller/OrderController.java
+++ order-service/src/main/java/ms/learnings/orderservice/controller/OrderController.java
@@ -1,5 +1,8 @@
 package ms.learnings.orderservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import ms.learnings.orderservice.dto.OrderRequest;
 import ms.learnings.orderservice.service.OrderService;
@@ -8,15 +11,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/order")
 public class OrderController {
     private final OrderService orderService;
     @PostMapping()
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order placed successfully";
+    @CircuitBreaker(name = "inventory", fallbackMethod = "placeOrderFallback")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return  CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    }
+
+
+    CompletableFuture<String> placeOrderFallback(OrderRequest orderRequest, Exception exception) {
+        return CompletableFuture.supplyAsync(() -> "Failed to place an order, please try again " + exception.getMessage());
     }
 }
 
