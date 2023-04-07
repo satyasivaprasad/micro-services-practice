cc37ff9ca03fc5b73f8d92800784d53d9c649f8e
satyasivaprasad <satyasivaprasad.j@gmail.com>
2023-04-07 14:10:52 +0530

    Circute breaker integration

--- inventory-service/src/main/java/ms/learnings/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/ms/learnings/inventoryservice/service/InventoryService.java
@@ -1,5 +1,7 @@
 package ms.learnings.inventoryservice.service;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import ms.learnings.inventoryservice.dto.InventoryResponse;
 import ms.learnings.inventoryservice.repository.InventoryRepository;
 import org.springframework.stereotype.Service;
@@ -10,12 +12,17 @@ import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(Set<String> skuCode) {
+        log.info("Wait Started");
+        Thread.sleep(10000);
+        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                         InventoryResponse.builder()
